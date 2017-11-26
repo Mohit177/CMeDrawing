7edc0884b24dde15a404a0fb84f3f1c5bc8ab005
nitishplus98 <nitishplus98@gmail.com>
2017-11-26 16:37:44 +0530

    Modify little

--- project/main.cpp
+++ project/main.cpp
@@ -6,12 +6,26 @@
 #include <cmath>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include "context.cpp"
 #define ll long long int
 
 using namespace std;
 
 int main()
 {
-		
+	Context context; //Creating an OpenGL context
+	context.initialize(); // initializing context
+	char name[]="My window";
+	context.createWindow(1200,700,name);
+	glfwSwapBuffers(context.getWindow());
+	glfwPollEvents();
+	//while the close button of window not pressed, the window keeps refreshing
+	while(!glfwWindowShouldClose(context.getWindow()))
+	{
+		//glDrawPixels(context.getWindowWidth(),context.getWindowHeight(),GL_RGB,GL_UNSIGNED_INT,buffer);
+		glfwSwapBuffers(context.getWindow());
+		glfwPollEvents();
+	}
+	context.terminate(); //destroys window
 	return 0;
 }
